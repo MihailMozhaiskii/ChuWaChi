9cb6a0abc9a641c2dadda2f4365387a6de1cd0a2
MykhailoMozhaiskyi <mmozhaiskyi@cogniance.com>
2019-06-25 23:27:30 +0300

    Base without design

---
+++ app/src/main/java/com/mmozhaiskyi/chuwachi/feature/ui/count/ChooseCountContentModel.kt
@@ -0,0 +1,12 @@
+package com.mmozhaiskyi.chuwachi.feature.ui.count
+
+sealed class ChooseCountContentModel(val type: ViewType) {
+
+    enum class ViewType { TITLE, MORE_BUTTON, NUMBER }
+
+    object Title : ChooseCountContentModel(ViewType.TITLE)
+
+    data class Number(val count: Int) : ChooseCountContentModel(ViewType.NUMBER)
+
+    object MoreButton : ChooseCountContentModel(ViewType.MORE_BUTTON)
+}
---
+++ app/src/main/java/com/mmozhaiskyi/chuwachi/feature/ui/count/ChooseCountContentViewHolder.kt
@@ -0,0 +1,63 @@
+package com.mmozhaiskyi.chuwachi.feature.ui.count
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+import com.mmozhaiskyi.chuwachi.R
+import kotlinx.android.synthetic.main.item_count_number.view.*
+
+sealed class ChooseCountContentViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    abstract fun bind(item: ChooseCountContentModel)
+
+    class Title private constructor(view: View) : ChooseCountContentViewHolder(view) {
+
+        override fun bind(item: ChooseCountContentModel) = Unit
+
+        companion object {
+
+            fun inflate(parent: ViewGroup): Title {
+                val view = inflateView(R.layout.item_title, parent)
+                return Title(view)
+            }
+        }
+    }
+
+    class Number private constructor(private val view: View) : ChooseCountContentViewHolder(view) {
+
+        override fun bind(item: ChooseCountContentModel) {
+            item as ChooseCountContentModel.Number
+            view.number.text = item.count.toString()
+        }
+
+        companion object {
+
+            fun inflate(parent: ViewGroup): Number {
+                val view = inflateView(R.layout.item_count_number, parent)
+                return Number(view)
+            }
+        }
+    }
+
+    class MoreButton private constructor(view: View) : ChooseCountContentViewHolder(view) {
+
+        override fun bind(item: ChooseCountContentModel) = Unit
+
+        companion object {
+
+            fun inflate(parent: ViewGroup): MoreButton {
+                val view = inflateView(R.layout.item_count_more, parent)
+                return MoreButton(view)
+            }
+        }
+    }
+
+    companion object {
+
+        private fun inflateView(@LayoutRes res: Int, parent: ViewGroup) = LayoutInflater
+            .from(parent.context)
+            .inflate(res, parent, false)
+    }
+}
